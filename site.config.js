f781c224263ae7f3315b9c1fe40f035033e38ccb
jarelion <jarelion@gmail.com>
2022-09-17 10:06:59 -0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '426aed2971104908a1f07d4cdab61d3b',
+  rootNotionPageId: '582ec58709b0449abf6dbf5e4bc53ea9',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Build websites with Notion',
-  domain: 'jotzilla.vercel.app',
-  author: 'Jotzilla',
+  name: 'jare.me',
+  domain: 'www.jare.me',
+  author: 'jp',
 
   // open graph metadata (optional)
-  description: 'Learn how to build websites with Notion. This website is an example starter site powered by Notion. Built on top of NotionX.',
-  socialImageTitle: 'Build websites with Notion',
-  socialImageSubtitle: 'Learn how to build websites with Notion. This website is an example starter site powered by Notion. Built on top of NotionX.',
+  description: 'thoughts, photos, and then some',
+  socialImageTitle: 'jare.me',
+  socialImageSubtitle: 'a journal',
 
   // social usernames (optional)
-  twitter: 'jotzilla',
-  github: 'jotzilla',
-  linkedin: 'jotzilla',
+  twitter: 'jarelion',
+  github: '',
+  linkedin: '',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
